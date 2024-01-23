eb5c2e8b55e97c71f3622a57f5ce97c9c2d8658b
= <jaityshaun20@gmail.com>
2024-01-22 20:28:02 -0600

    initialCommit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+/*for(let x=1;x<=100;x++){
+    if (x%2==0){
+        console.log('Even'+ x)
+    }
+    if (x%2!=0){
+        console.log('Odd'+x)
+    }
+}*/
+
+let _ = require('lodash');
+let myVariable = 'go big red';
+console.log(_.camelCase(myVariable));
